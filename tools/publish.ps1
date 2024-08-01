88999a8fdb0ab34afb9c9dbee78729c557aeba3a
WRC <hello_wrc@outlook.com>
2024-08-01 15:27:46 +0800

    build: 适配自动化构建

---
+++ tools/publish.ps1
@@ -0,0 +1,21 @@
+﻿$PUBLISH_TARGET = "..\out\ClassIsland.PluginIndexGenerator"
+
+if ($(Test-Path ./out) -eq $false) {
+    mkdir out
+} else {
+    rm out/* -Recurse -Force
+}
+
+
+    
+dotnet publish .\ClassIsland.PluginIndexGenerator\ClassIsland.PluginIndexGenerator.csproj -c Release -p:PublishProfile=FolderProfile -p:PublishDir=$PUBLISH_TARGET -property:DebugType=embedded
+
+Write-Host "Successfully published to $PUBLISH_TARGET" -ForegroundColor Green
+
+Write-Host "Packaging..." -ForegroundColor Cyan
+
+rm ./out/ClassIsland.PluginIndexGenerator/*.xml
+
+7z a "./out/ClassIsland.PluginIndexGenerator.zip" ./out/ClassIsland.PluginIndexGenerator/* -r -mx=9
+
+rm -Recurse -Force ./out/ClassIsland.PluginIndexGenerator
